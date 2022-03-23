4a1b38e6e1c2dfe87987ab47baff94e206600032
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-03-23 16:58:07 -0700

    add form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
-var taskToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskNameEl = document.querySelector('input[name="task-name"]')
 
-var createTaskHandler = function(){
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "New list item";
-    taskItemEl.className = "task-item";
-    taskToDoEl.appendChild(taskItemEl)
-};
-
-
-buttonEl.addEventListener("click", createTaskHandler)
+var createTaskHandler = function(event) { 
+    event.preventDefault(); 
+  
+    var listItemEl = document.createElement("li"); 
+    listItemEl.className = "task-item"; 
+    listItemEl.textContent = "This is a new task."; 
+    tasksToDoEl.appendChild(listItemEl); 
+    }; 
+  
+    formEl.addEventListener("submit", createTaskHandler);
